4eed1772190fbc1a864017252f2d0b99f9009e1b
Nilejames <nileena.james@netapp.com>
2024-11-26 13:44:59 +0530

    Update jiraCreateIssue.groovy

--- vars/jiraCreateIssue.groovy
+++ vars/jiraCreateIssue.groovy
@@ -7,5 +7,5 @@ def call(Map config=[:]) {
     issueTypeName: "${config.issueTypeName}"
   ]
   def render = renderTemplate(rawBody,binding)
-  sh('curl -D- -u Bearer $JIRA_CREDENTIALS -X POST --data "'+render+'" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue')
+  sh('curl -D-  -H "Authorization: Bearer "$JIRA_CREDENTIALS -X POST --data "'+render+'" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue')
 }
